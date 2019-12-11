d94ad89e07a67199262672332f04db469d6b68f4
Koray Oezkaynak <koray.oezkaynak@students.fhnw.ch>
2019-12-11 18:15:28 +0100

    masterlayout almost finished - footer need improvement!

---
+++ js/custom.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    
+    if ($(window).scrollTop() >= 100) {
+        
+      $('.navbar').addClass('scrolled-nav');
+        
+        if($('.nav-link, .navbar-brand').removeClass('text-dark')){
+          $('.nav-link, .navbar-brand').addClass('text-light');
+        }
+        
+        if($('.navbar-toggler').removeClass('navbar-light')){
+          $('.navbar-toggler').addClass('navbar-dark');
+        }
+      } else if (scrollTop < 100) {
+        $('.navbar').removeClass('scrolled-nav');
+      }
+    
+    var scrollTop = 0;
+    $(window).scroll(function(){
+      scrollTop = $(window).scrollTop();
+       $('.counter').html(scrollTop);
+      
+      if (scrollTop >= 100) {
+        $('.navbar').addClass('scrolled-nav');
+        
+        if($('.nav-link, .navbar-brand').removeClass('text-dark')){
+          $('.nav-link, .navbar-brand').addClass('text-light');
+        }
+
+        if($('.navbar-toggler').removeClass('navbar-light')){
+          $('.navbar-toggler').addClass('navbar-dark');
+        }
+
+
+      } else if (scrollTop < 100) {
+        
+        $('.navbar').removeClass('scrolled-nav');
+        
+        if($('.nav-link, .navbar-brand').removeClass('text-light')){
+          $('.nav-link, .navbar-brand').addClass('text-dark');
+        }
+
+        if($('.navbar-toggler').removeClass('navbar-dark')){
+          $('.navbar-toggler').addClass('navbar-light');
+        }
+      } 
+      
+    });    
+});
