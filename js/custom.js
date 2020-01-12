550380f95ef68d907561715c24931bb8db70b0d4
Redoon <korayoezkaynak@gmail.com>
2020-01-12 16:47:39 +0100

    all sites aviable

--- js/custom.js
+++ js/custom.js
@@ -6,10 +6,16 @@ $(document).ready(function(){
         
         if($('.nav-link, .navbar-brand').removeClass('text-dark')){
           $('.nav-link, .navbar-brand').addClass('text-light');
+
+          $('.nav-item').removeClass('nav-custom-hover-dark');
+          $('.nav-item').addClass('nav-custom-hover-light');
         }
         
         if($('.navbar-toggler').removeClass('navbar-light')){
           $('.navbar-toggler').addClass('navbar-dark');
+
+          $('.nav-item').removeClass('nav-custom-hover-light');
+          $('.nav-item').addClass('nav-custom-hover-dark');
         }
       } else if (scrollTop < 80) {
         $('.navbar').removeClass('scrolled-nav');
@@ -18,7 +24,6 @@ $(document).ready(function(){
     var scrollTop = 0;
     $(window).scroll(function(){
       scrollTop = $(window).scrollTop();
-       $('.counter').html(scrollTop);
       
       if (scrollTop >= 80) {
         $('.navbar').addClass('scrolled-nav');
@@ -29,6 +34,9 @@ $(document).ready(function(){
 
         if($('.navbar-toggler').removeClass('navbar-light')){
           $('.navbar-toggler').addClass('navbar-dark');
+          
+          $('.nav-item').removeClass('nav-custom-hover-light');
+          $('.nav-item').addClass('nav-custom-hover-dark');
         }
 
 
@@ -38,10 +46,16 @@ $(document).ready(function(){
         $('.nav-logo').removeClass('scrolled-nav-logo');
         if($('.nav-link, .navbar-brand').removeClass('text-light')){
           $('.nav-link, .navbar-brand').addClass('text-dark');
+
+          $('.nav-item').removeClass('nav-custom-hover-light');
+          $('.nav-item').addClass('nav-custom-hover-dark');
         }
 
         if($('.navbar-toggler').removeClass('navbar-dark')){
           $('.navbar-toggler').addClass('navbar-light');
+
+          $('.nav-item').removeClass('nav-custom-hover-dark');
+          $('.nav-item').addClass('nav-custom-hover-light');
         }
       } 
       
